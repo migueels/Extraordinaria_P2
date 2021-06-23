e083d8019f4bb12943b59847a1f161337b26f2f2
Miguel <salasrmcf1997@gmail.com>
2021-06-23 10:13:10 +0200

    actualizacion POM

---
+++ src/test/java/com/disVaadin/Practica2Ext/MainViewTest.java
@@ -0,0 +1,20 @@
+package com.disVaadin.Practica2Ext;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainViewTest {
+
+    @Test
+    void guardadoJson() {
+    }
+
+    @Test
+    void nuevoHeroeModal() {
+    }
+
+    @Test
+    void nuevasBatallasModal() {
+    }
+}
